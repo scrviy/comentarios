9e06ed9c32f011cda3f31bf2f8b79f4433cc5718
scrviy <91909966+scrviy@users.noreply.github.com>
2023-01-13 20:04:11 -0300

    add doc about comments

---
+++ comment.js
@@ -0,0 +1,14 @@
+// Comentário
+
+/* Comentario de varias linhas
+* - auau
+* - miau
+* - copa pistao
+*/
+console.log("Hello, World!");
+console.log("Linha 1");
+console.log("Linha 2");
+console.log("Linha 3");
+console.log("Linha 4");
+
+// Linha ignorada. 
